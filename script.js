202afea3c18680297886e08aa0e3eab8c9a6753c
shax911 <82878451+shax911@users.noreply.github.com>
2021-04-29 20:02:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = prompt ('введите имя');
+console.log ('ваше имя: ' + name);
+var year = +prompt ('введите возраст');
+console.log('ваш возраст : ' + year);
+var num = +prompt('10 + 3 =');
+console.log('10 + 3 = ' + (10 + 3) + ': (ваш ответ ' + num + ')');
+var num1 = +prompt('10 - 3 =')
+console.log('10 - 3 = ' + (10 - 3) + ': (ваш ответ ' + num1 + ')');
+var num2 = +prompt('10 / 3 =')
+console.log('10 / 3 = ' + (10 / 3) + ': (ваш ответ ' + num2 + ')');
+var num3 = +prompt('10 * 3 =')
+console.log('10 * 3 = ' + (10 * 3) + ': (ваш ответ ' + num3 + ')');
+var num4 = +prompt('10 % 3 =')
+console.log('10 % 3 = ' + (10 % 3) + ': (ваш ответ ' + num4 + ')');
+var finish = 'перейдите в консоль за ответами';
+alert(finish)
